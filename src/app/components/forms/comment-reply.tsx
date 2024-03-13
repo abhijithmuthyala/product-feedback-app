0b283504bac1526b326e7619240b03d1c98a2537
Abhijith Muthyala <73361366+abhijithmuthyala@users.noreply.github.com>
2024-03-13 21:58:34 +0530

    Wrap comment post in a transition for better ux

--- src/app/components/forms/comment-reply.tsx
+++ src/app/components/forms/comment-reply.tsx
@@ -1,9 +1,17 @@
+"use client";
+
+import { useTransition } from "react";
 import PostCommentButton from "../buttons/post-comment";
 import TextArea from "../text-area";
 
 export default function CommentReplyForm({ onSuccess, action }) {
+  // using a transition results in a better ux - the new
+  // comment appears immediately after the form is closed.
+  // TODO: THIS FEELS LIKE A BLACK BOX. EXPLORE MORE!
+  const [pending, startTransition] = useTransition();
+
   async function postReply(formData) {
-    await action(formData);
+    startTransition(action(formData));
     onSuccess();
   }
 
