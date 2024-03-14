26befcd1bba0814372e09d7dcd54b6d908cc1dae
Abhijith Muthyala <73361366+abhijithmuthyala@users.noreply.github.com>
2024-03-14 19:26:58 +0530

    Render comments section only there is atleast one

--- src/app/components/comments/comments.tsx
+++ src/app/components/comments/comments.tsx
@@ -22,24 +22,27 @@ export default async function Comments({ postId }) {
 
   return (
     <>
-      <section className="rounded-md bg-neutral-0 px-6 py-6 md:px-8 md:pb-8">
-        <h2 className="text-lg font-bold text-neutral-7">
-          {numberOfComments} Comments
-        </h2>
-        <div className="mt-6 grid gap-y-6 md:mt-7">
-          <LevelContextProvider>
-            {comments.map(function renderComment(data, index) {
-              return (
-                <Comment
-                  key={data.id}
-                  data={data}
-                  isLastComment={index === comments.length - 1}
-                />
-              );
-            })}
-          </LevelContextProvider>
-        </div>
-      </section>
+      {numberOfComments > 0 && (
+        <section className="rounded-md bg-neutral-0 px-6 py-6 md:px-8 md:pb-8">
+          <h2 className="text-lg font-bold text-neutral-7">
+            {numberOfComments} Comments
+          </h2>
+          <div className="mt-6 grid gap-y-6 md:mt-7">
+            <LevelContextProvider>
+              {comments.map(function renderComment(data, index) {
+                return (
+                  <Comment
+                    key={data.id}
+                    data={data}
+                    isLastComment={index === comments.length - 1}
+                  />
+                );
+              })}
+            </LevelContextProvider>
+          </div>
+        </section>
+      )}
+
       {isAuthenticated && (
         <div className="mt-6 rounded-md bg-neutral-0 p-6 md:p-8">
           <h2 className="mb-6 text-lg font-bold text-neutral-7">Add Comment</h2>
