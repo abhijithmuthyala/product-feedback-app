e8df10ade116ef16aa52dfc9af7899c7e5ce9fe0
Abhijith Muthyala <73361366+abhijithmuthyala@users.noreply.github.com>
2024-03-14 20:25:41 +0530

    Render feedback request ui when suggestions are empty

--- src/app/components/new-suggestion-link.tsx
+++ src/app/components/new-suggestion-link.tsx
@@ -4,7 +4,7 @@ export default function NewSuggestionLink() {
   return (
     <Link
       href="/"
-      className="inline-flex items-center gap-2 rounded-lg bg-primary px-4 py-2.5 font-bold"
+      className="inline-flex items-center gap-2 rounded-lg bg-primary px-4 py-2.5 text-sm font-bold text-neutral-1"
     >
       <span className="aspect-square w-3 bg-plus bg-contain bg-no-repeat"></span>
       <span>Add Feedback</span>
