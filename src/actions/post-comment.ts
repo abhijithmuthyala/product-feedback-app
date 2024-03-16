64e03c2c9af8f55c4480d6c0e33b691bf98d348d
Abhijith Muthyala <73361366+abhijithmuthyala@users.noreply.github.com>
2024-03-17 00:56:39 +0530

    Fix username tag in replies and order of comments

--- src/actions/post-comment.ts
+++ src/actions/post-comment.ts
@@ -3,12 +3,18 @@
 import { createClient } from "@/supabase/server";
 import { revalidatePath } from "next/cache";
 
-export async function postCommentReply(parentID, postID, formData) {
+export async function postCommentReply(
+  parentID,
+  postID,
+  replyingToID,
+  formData,
+) {
   const supabase = createClient();
   const { data, error } = await supabase.rpc("insert_comment", {
     comment_param: formData.get("comment"),
     parent_id_param: parentID,
     post_id_param: postID,
+    replying_to_id_param: replyingToID,
   });
 
   if (error) console.error(error);
