353aad84bae3a8bf5fe842175e2a6d9b1abf6edd
Abhijith Muthyala <73361366+abhijithmuthyala@users.noreply.github.com>
2024-03-16 23:31:19 +0530

    Add padding around login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import AuthForm from "../components/auth-form";
 
 export default async function LoginPage() {
   return (
-    <main className="py-12">
+    <main className="min-h-screen px-6 py-12">
       <section className="mx-auto max-w-lg rounded-lg bg-neutral-1 px-6 py-6 shadow-md shadow-neutral-4">
         <AuthForm />
       </section>
