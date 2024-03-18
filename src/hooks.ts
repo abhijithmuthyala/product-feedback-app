6f32ecb50520528f3df75f0ee28d8ae09a2fc6a0
Abhijith Muthyala <73361366+abhijithmuthyala@users.noreply.github.com>
2024-03-18 21:10:43 +0530

    Fix query param effect adding a new entry in the history stack

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,16 +1,12 @@
 import { useEffect } from "react";
 
 export function useUpdateQueryParamEffect(key, value) {
-  useEffect(
-    function updateQueryParam() {
-      const searchParams = new URLSearchParams(location.search);
-      searchParams.set(key, value);
-      history.pushState(
-        null,
-        "",
-        location.pathname + "?" + searchParams.toString(),
-      );
-    },
-    [key, value],
-  );
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    searchParams.set(key, value);
+
+    const newUrl = `${window.location.pathname}?${searchParams.toString()}`;
+
+    window.history.replaceState(null, "", newUrl);
+  }, [key, value]);
 }
