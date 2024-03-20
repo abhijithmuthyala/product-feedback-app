4f46c52d0984138373690043f764650d131b36e9
Abhijith Muthyala <73361366+abhijithmuthyala@users.noreply.github.com>
2024-03-20 14:38:38 +0530

    Increase hit area for feedback link

--- src/app/components/suggestions/suggestion.tsx
+++ src/app/components/suggestions/suggestion.tsx
@@ -9,23 +9,18 @@ export default async function Suggestion({ data, asLink = false }) {
     data.title[0],
     data.title[0].toUpperCase(),
   );
-  const titleElement = (
-    <h2 className="font-bold text-neutral-7 sm:text-lg">
-      {asLink ? (
-        <Link href={`/details/${data.id}`}>{formattedTitle}</Link>
-      ) : (
-        formattedTitle
-      )}
-    </h2>
+
+  const header = (
+    <header className="flex flex-col gap-2 md:gap-1">
+      <h2 className="font-bold text-neutral-7 sm:text-lg">{formattedTitle}</h2>
+      <p className="text-neutral-6 sm:text-base">{data.description}</p>
+    </header>
   );
 
   return (
     <article className="grid grid-cols-[auto_1fr_auto] grid-rows-[auto_auto] gap-y-4 rounded-lg bg-neutral-0 px-6 py-6 text-sm sm:grid-rows-1 sm:gap-x-10 md:px-8 md:py-7">
       <div className="flex flex-col items-start gap-2 max-sm:col-span-full sm:col-start-2 sm:row-start-1">
-        <header className="flex flex-col gap-2 md:gap-1">
-          {titleElement}
-          <p className="text-neutral-6 sm:text-base">{data.description}</p>
-        </header>
+        {asLink ? <Link href={`/details/${data.id}`}>{header}</Link> : header}
         <CategoryTag category={data.category} />
       </div>
       <div className="sm:row-start-1">
